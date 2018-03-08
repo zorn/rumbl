1aa5c3e269bf24c1d1d2b473abee68cd404cbeab
Mike Zornek <zorn@zornlabs.com>
2018-03-08 16:12:41 -0500

    Current state of project.

---
+++ config/prod.secret.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :rumbl, Rumbl.Endpoint,
+  secret_key_base: "j+w9Un4xB/xMjBl34hsDkm/QD+uHjgbE3VcBUpLC0qQG402KdYGM2hru61trTzVR"
+
+# Configure your database
+config :rumbl, Rumbl.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "rumbl_prod",
+  pool_size: 20
---
+++ web/router.ex
@@ -0,0 +1,21 @@
+defmodule Rumbl.Router do
+  use Rumbl.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Rumbl do
+    pipe_through :browser
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+  end
+end
