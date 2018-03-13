6dfe5d94061148ddbae59d4a3f3771bdcfc986b1
Mike Zornek <zorn@zornlabs.com>
2018-03-13 18:44:16 -0400

    End of chapter 5.

--- web/models/user.ex
+++ web/models/user.ex
@@ -10,7 +10,7 @@ defmodule Rumbl.User do
   	timestamps()
   end
 
-  def changeset(model, params \\ :empty) do 
+  def changeset(model, params) do 
   	# model
   	# |> cast(params, ~w(name username), [])
   	# |> validate_length(:username, min: 1, max: 20)
@@ -19,6 +19,24 @@ defmodule Rumbl.User do
     |> cast(params, ~w(name username)) 
     |> validate_required([:name, :username]) 
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password))
+    |> validate_required([:password]) 
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 
   #defstruct [:id, :name, :username, :password]
